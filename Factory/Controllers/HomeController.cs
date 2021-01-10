be844e8dcd3f2643c0a6c50fc0ea834c85ae6ed5
taylulz <taylulzcode@gmail.com>
2021-01-10 11:15:47 -0800

    working model on splash page w/two lists

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+using Factory.Models;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
     public ActionResult Index()
     {
-      return View();
+      var AllEngineers = _db.Engineers.ToList();
+      var AllMachines = _db.Machines.ToList();
+      var viewModel = new MyViewModel();
+      viewModel.AllEngineers = AllEngineers;
+      viewModel.AllMachines = AllMachines;
+      return View(viewModel);
     }
   }
 }
---
+++ Factory/Models/MyViewModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class MyViewModel
+  {
+    public List<Engineer> AllEngineers { get; set; }
+    public List<Machine> AllMachines { get; set; }
+  }
+}
